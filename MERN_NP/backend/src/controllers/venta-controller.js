12ed67995cc9ae4ff1c06dc5d1677fe342a8fc7b
= <=>
2021-10-14 15:28:52 -0500

    Organizacion de ramas

--- MERN_NP/backend/src/controllers/venta-controller.js
+++
@@ -1,9 +0,0 @@
-const ventaCtrl={};
-
-ventaCtrl.getVentas=(request,response)=> response.json({message: []})
-ventaCtrl.createVenta=(request,response)=> response.json({message: "Usuario creado"});
-ventaCtrl.getVenta = (request,response)=> response.json({title: "2121548"})
-ventaCtrl.updateVenta = (request,response)=> response.json({message: "usuario actualizada"})
-ventaCtrl.deleteVenta = (request,response)=> response.json({message: "usurio eliminada"})
-
-module.exports = ventaCtrl;
--- MERN_NP/backend/src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-require ("./database");
-
-async function main(){
-    await app.listen(app.get("port")); 
-    console.log ("Servidor en el puerto 4000");
-}
-
-main();
