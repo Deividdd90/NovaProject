6e8e6efd9aa01e90d1a9b7b5989e9a4c37b7a56a
Jhon Alexander Vallejo Briceño <jhon.vallejo@comware.com.co>
2021-10-16 09:50:13 -0500

    se ajusta agrega inicio de sesion

--- MERN_NP/frontend/src/components/auten/inisession.js
+++ MERN_NP/frontend/src/components/auten/inisession.js
@@ -1,10 +1,21 @@
 import  GoogleLogin  from 'react-google-login';
+import axios from "axios";
 
 
 const LoginGoogle = () => {
 
-    const respGoogle =(resp) => {
+    const respGoogle = async (resp) => {
         console.log(resp);
+
+        const newUser = {
+        Nombre:resp.profileObj.name,
+        Email:resp.profileObj.email
+        }
+        
+        const datosUser = await axios.post("http://localhost:4000/api/usuarios", newUser);
+      
+        console.log(datosUser);
+
     }
 
     return    <GoogleLogin
@@ -13,6 +24,7 @@ const LoginGoogle = () => {
         onSuccess={respGoogle}
         onFailure={respGoogle}
         cookiePolicy={'single_host_origin'} />
+
 }
 
 export default LoginGoogle;
